e708754f5fbaaa4115fe6e5a27be61955b89f326
Grzegorz Koza <kozagrzegorz@interia.pl>
2017-12-04 17:00:07 +0100

    Button text

---
+++ README.md
@@ -0,0 +1,2 @@
+#zadanie-9.5
+https://gelu4.github.io/zadanie-9.5/
---
+++ script.js
@@ -0,0 +1,7 @@
+var bt = document.getElementsByClassName('button');
+var len = bt.length;
+
+for( var i=0; i<len; i++) {
+	console.log(bt[i].innerText);
+	
+}
